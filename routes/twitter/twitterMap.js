0932e7ca82e3bfb03947ea98404846ac81d66c5f
solarprominence <igor.fsp.milla@gmail.com>
2014-02-25 23:04:46 +0000

    strange commit with files we didn't add in previos commit

---
+++ routes/twitter/twitterMap.js
@@ -0,0 +1,24 @@
+exports.getTweets = function(req, res){
+  var mongoose = require('mongoose');
+  mongoose.connect('mongodb://localhost/test');
+
+  
+  var db = mongoose.connection;
+  
+  var Tweet = require('../../models/tweet').Tweet;
+  
+  Tweet.find({}, function (err, tweets) {
+   var tweetsArray = [];
+   tweetsArray.push(['latitude', 'longitude', 'tweet', 'size']); 
+   tweets.map(function (tweet) {
+        var t = tweet.toObject();
+        tweetsArray.push([tweet.coordinates[1], tweet.coordinates[0], tweet.tweet, 1]);
+    });
+    db.close();
+    var response = {};
+    response.tweets = tweetsArray;
+    res.render('partials/map/lonelyPeople', response);
+  });
+  
+
+};
