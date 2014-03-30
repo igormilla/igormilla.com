30ec9827484d6046a4d9731be10e7a73ffb0137b
solarprominence <igor.fsp.milla@gmail.com>
2014-03-30 22:48:18 +0000

    adding an youtube link for the currently listened song

--- routes/lastfm.js
+++ routes/lastfm.js
@@ -1,16 +1,14 @@
-
-
 exports.getStream = function( req, res ){
-	
-	var lastfmNode = require('lastfm').LastFmNode,
+	var youtube = require('youtube-node');
+	youtube.setKey('AIzaSyCLfpvvT2h0QMa4ZwQCZW_uwJr1hEq_vTc');
 
-	lastfm = new lastfmNode({
+	var lastfmNode = require('lastfm').LastFmNode;
+	var lastfm = new lastfmNode({
 	  api_key	: 'a3673aaa4a4e2090e0f61663c8a7fb4e',
 	  secret	: 'd2ccc6f1deb3a1e37e5c5fdb53f60189',
 	  useragent	: 'appname/igormilla.com' 
-	}),
-	
-	trackStream = lastfm.stream('s_ashen');
+	});
+	var trackStream = lastfm.stream('s_ashen');
 
 	function lastfmStreamCallback(track) {
 		//trackStream.stop();		
@@ -20,9 +18,16 @@ exports.getStream = function( req, res ){
 		data.artist = track.artist['#text'];
 		data.url = track.url;
 		data.date = track.date['uts'];
-		res.render('partials/lastfm/lastfm-stream', data);
+		youtube.search(data.artist + ' ' + data.track, 1, function(resultData){
+			if(resultData.items.length > 0){
+				console.log(resultData.items[0].id.videoId);
+				data.youtube = resultData.items[0].id.videoId;
+			}
+			res.render('partials/lastfm/lastfm-stream', data);
+		});
+
 	};
-	
+
 	trackStream
 		.on('lastPlayed', lastfmStreamCallback)
 		.start();
